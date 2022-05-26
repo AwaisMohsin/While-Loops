e91e55c68ddd30c63ad72083e1fcac185c37bba8
Awais <Awaisbinmohsin@gmail.com>
2022-05-27 00:32:46 +0500

    While Loops

---
+++ Index.js
@@ -0,0 +1,18 @@
+// let i = prompt("Please enter value");
+// if (i < 5) {
+//     while (i < 5) {
+//         console.log(i);
+//         i++;
+//     }
+// }
+
+//Counting
+// let a = prompt("Please Enter value");
+// if(!isNaN(a)){
+//     if (a <= 100){
+//         while(a<=100){
+//             console.log(a);
+//             a++;
+//         }
+//     }
+// }
